1ca49ddd6ffef2e9bb10200e35d675373429231e
GUILHERME HENRIQUE LUIZ EUZEBIO PEREIRA <1A226881906@fumeccad.local>
2022-09-14 10:53:04 -0300

    build: inserting components - login page and sign up page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SignUpPageComponent
   ],
   imports: [
     BrowserModule,
