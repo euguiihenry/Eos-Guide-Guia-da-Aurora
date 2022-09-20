516ca288fcd5e9d66208eb31c154a56f3d15f229
GUILHERME HENRIQUE LUIZ EUZEBIO PEREIRA <1A226881906@fumeccad.local>
2022-09-20 11:22:32 -0300

    style: updating home-page style and its components

--- src/app/home/footer/footer.component.ts
+++ src/app/home/footer/footer.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss',
+              '../../../scss/abstracts/_mixins.scss',
+              '../../../scss/components/_typography.scss',
+              '../../../scss/abstracts/_variables.scss'
+  ]
 })
 export class FooterComponent implements OnInit {
 
