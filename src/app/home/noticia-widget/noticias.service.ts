2c4f80578842aa37f5560788cbe6b64196ccbe9d
Guilherme Henrique <gui2012451@gmail.com>
2022-09-25 10:33:06 -0300

    fix: fixing error from fetch data using API in news component

--- src/app/home/noticia-widget/noticias.service.ts
+++ src/app/home/noticia-widget/noticias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { NewsInterface } from './noticias';
 
 @Injectable({
@@ -9,15 +10,25 @@ export class NoticiasService {
 
   private readonly connection = "https://gnews.io/api/v4/{endpoint}?token=67cf13a832ba3a73fce52db137b964ff";
 
-  constructor(private http: HttpClient) { }
+  private readonly params = new HttpParams()
+  
+
+  constructor(private http: HttpClient) {
+    this.params.set('topic', "breaking-news");
+    this.params.set('lang', "pt");
+    this.params.set('country', "br",);
+    this.params.set('max', "4");
+  }
 
   list() {
     return this.http.get<NewsInterface[]>(this.connection, {
+      observe: 'body',
+      responseType: 'json',
       params: {
-        topic: "breaking-news",
-        lang: "pt",
-        country: "br",
-        max: "4"
+        topic: 'breaking-news',
+        lang: 'pt',
+        country: 'br',
+        max: '4'
       }
     });
   }
