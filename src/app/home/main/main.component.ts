266013d74a6e981fa8eff1a6a80ac85d49f1a8a2
GUILHERME HENRIQUE LUIZ EUZEBIO PEREIRA <1A226881906@fumeccad.local>
2022-10-04 11:06:38 -0300

    build: rebuilding main component and remove some components from home

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -6,9 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  local: string;
+  iconName: string;
+  bootstrapWeatherIcon: string;
 
-  constructor() { }
+  windP: number;
+  waterP: number;
+  rainP: number;
 
+  temperatureStatus: string;
+  degrees: number;
+
+  musicName: string;
+  singer: string;
+  statusIcon: string;
+  statusOfPlayer: string;
+
+
+  constructor() { 
+    this.local = "Belo Horizonte";
+    this.iconName = "cloud-sun";
+    this.bootstrapWeatherIcon = "bootstrap-icons.svg#" + this.iconName;
+
+    this.windP = 10;
+    this.waterP = 39;
+    this.rainP = 0;
+
+    this.temperatureStatus = "Parcialmente nublado";
+    this.degrees = 25;
+
+    this.musicName = "Physical";
+    this.singer = "Dua Lipa";
+    this.statusIcon = "play-circle-fill"
+    this.statusOfPlayer = "bootstrap-icons.svg#" + this.iconName;
+  }
+  
   ngOnInit(): void {
   }
 
