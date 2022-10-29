cc71ef57601ee9ae02ef664a21304270a1accce9
Guilherme Henrique <gui2012451@gmail.com>
2022-10-29 17:17:03 -0300

    feat: implementing an error message when not getting news from server

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NoticiasService } from '../services/news/noticias.service';
 //import { newsDb } from '../services/news/newsDb.json';
 import { NewsInterface } from '../services/news/models/proto-news-interface';
-import { FinalNewsInterface } from '../services/news/models/final-news-interface';
 
 @Component({
   selector: 'app-main',
@@ -48,59 +47,6 @@ export class MainComponent implements OnInit {
   // News variables:
     public protoNews: NewsInterface;
     public newsArticles: any;
-    public showNewsErrorMsg: Boolean;
-
-  // Simulação de um dados trazido até a variável pela API:
-    public noticiasTeste: any[] = [
-      {
-        title: "Profissionalização e investimento até em redes sociais: como o Corinthians virou potência no futebol feminino",
-        description: 'Timão amplia hegemonia ao conquistar o quarto título (terceiro seguido) do Brasileirão',
-        content: 'É a 12ª conquista de relevância (estadual, nacional ou continental) das Brabas do Timão — elas também levantaram o troféu da competição nacional em 2018, 2020 e 2021.\nMas o que explica tamanha imponência do Corinthians no futebol feminino?\n1 de 5 Jog...',
-        url: 'https://ge.globo.com/futebol/times/corinthians/noticia/2022/09/25/profissionalizacao-e-investimento-ate-em-redes-sociais-como-o-corinthians-virou-potencia-no-futebol-feminino.ghtml',
-        image: 'https://s2.glbimg.com/M2fcnim2R2UImHmBCqWgyUMgBrA=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/s/x/P2rPHURQGsYoWhBbB1FA/rib1071-3.jpg',
-        publishedAt: '2022-09-25T10:36:01Z',
-        source: {
-            name: 'Globo',
-            url: 'https://ge.globo.com'
-        }
-      },
-      {
-        title: 'O carrinho encolheu: cariocas contam o que mudaram nos hábitos de consumo na hora das compras',
-        description: 'Os brasileiros estão consumindo menos e pagando mais nos supermercados do Rio de Janeiro, é cada vez mais raro ver carrinhos de compras cheios, mesmo nos fins..."',
-        content: 'Rodinei Sampaio, sai do Anadarí para fazer compras em Cascadura para aproveitar promoção Foto: Fabio Rossi / Agência O Globo\nMartha Imenes Tamanho do texto\nOs brasileiros estão consumindo menos e pagando mais nos supermercados do Rio de Janeiro, é ca...',
-        url: 'https://extra.globo.com/economia-e-financas/o-carrinho-encolheu-cariocas-contam-que-mudaram-nos-habitos-de-consumo-na-hora-das-compras-25575779.html',
-        image: 'https://extra.globo.com/economia-e-financas/25576929-d88-6e6/w976h550-PROP/100590060.jpg',
-        publishedAt: '2022-09-25T09:00:00Z',
-        source: {
-            name: 'Extra',
-            url: 'https://extra.globo.com'
-        }
-      },
-      {
-        title: "Aline Borges celebra auge da carreira com 'Pantanal' e relembra infância pobre: 'Andei de elevador pela primeira vez aos 9 anos'",
-        description: 'Antes de falar sobre qualquer assunto nesta entrevista, Aline Borges quis registrar um agradecimento público aos pais, Maria Helena, de 80 anos, e Mariano...',
-        content: 'Gabriela Medeiros Tamanho do texto\nAntes de falar sobre qualquer assunto nesta entrevista, Aline Borges quis registrar um agradecimento público aos pais, Maria Helena, de 80 anos, e Mariano Borges, de 92, leitores assíduos do EXTRA: “Eu não seria nad...',
-        url: 'https://extra.globo.com/tv-e-lazer/aline-borges-celebra-auge-da-carreira-com-pantanal-relembra-infancia-pobre-andei-de-elevador-pela-primeira-vez-aos-9-anos-25577354.html',
-        image: 'https://extra.globo.com/incoming/25576750-b45-e00/w976h550-PROP/aline-borges.jpg',
-        publishedAt: '2022-09-25T08:00:00Z',
-        source: {
-            name: 'Extra',
-            url: 'https://extra.globo.com'
-        }
-      },
-      {
-        title: "Aliados de Putin expressam preocupação com 'excessos' de mobilização",
-        description: "Aliados de Putin expressam preocupação com 'excessos' de mobilização",
-        content: '1 de 3 O presidente russo Vladmir Putin durante reunião em 10 de setembro — Foto: Sputnik/Mikhail Klimentyev/Kremlin via Reuters\nOs dois parlamentares mais importantes da Rússia abordaram neste domingo (25) uma série de queixas sobre a campanha de mo...',
-        url: 'https://g1.globo.com/mundo/ucrania-russia/noticia/2022/09/25/aliados-de-putin-expressam-preocupacao-com-excessos-de-mobilizacao.ghtml',
-        image: 'https://s2.glbimg.com/6f-1nbb0UEdcxOZmGpQYyXV6l-8=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/D/o/f0zxB9Q8WsFDoch8wRaw/2020-09-10t115240z-1203852726-rc2nvi933mlu-rtrmadp-3-russia-putin.jpg',
-        publishedAt: '2022-09-25T08:59:10Z',
-        source: {
-            name: 'Globo',
-            url: 'https://g1.globo.com'
-        }
-      }
-    ];
 
   constructor(private newsService: NoticiasService) { 
 
@@ -148,7 +94,6 @@ export class MainComponent implements OnInit {
     // News:
       this.protoNews  = {} as NewsInterface;
       this.newsArticles = '';
-      this.showNewsErrorMsg = false;
 
   }
 
@@ -158,19 +103,11 @@ export class MainComponent implements OnInit {
     .subscribe((newsType: NewsInterface) => {
       this.protoNews = newsType;
       this.newsArticles = this.protoNews.articles.map(article => article);
-
-      if(this.newsService.showErrorMsgForUser) {
-        this.showNewsErrorMsg = true;
-      }
     })
   }
   
   ngOnInit(): void {
     this.getNewsDB();
-
-    if(this.protoNews = {} as NewsInterface) {
-      this.showNewsErrorMsg = false;
-    }
   }
 
 }
--- src/app/home/services/news/noticias.service.ts
+++ src/app/home/services/news/noticias.service.ts
@@ -12,16 +12,10 @@ export class NoticiasService {
 
   //private readonly connection = "https://gnews.io/api/v4/top-headlines?token=67cf13a832ba3a73fce52db137b964ff&topic=breaking-news&lang=pt&country=br&max=4";
   private readonly connection = 'http://localhost:3000/news';  
-  public showErrorMsgForUser = false;
 
   constructor(private http: HttpClient) {}
 
   public getAllNews = () => this.http.get<NewsInterface>(this.connection);
 
   public getOneNews = (id: number) => this.http.get<NewsInterface>(this.connection+'/'+id);
-
-  public handleError(error:any) {
-    let errMsg = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    this.showErrorMsgForUser = true;
-  }
 }
