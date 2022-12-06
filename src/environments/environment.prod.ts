62bc86a0bcc6fc66ec03047ac0dc36cb1028d429
Henry <gui2012451@gmail.com>
2022-12-06 20:57:45 -0300

    updating variables of environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   openWeather: 'a5cf07b31ef30d7f5a16d1df55135522',
   musicAPI: 'e2762bf16amsh93c9f365d08ea17p10b0d0jsn210bd939dde6',
-  newsAPI: process.env["newsAPI"]
+  //@ts-ignore
+  newsAPI: process.env.newsAPI
 };
