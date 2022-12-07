b5ad06366c57af46478fc4fe3576388f6b5f3ed0
Henry <gui2012451@gmail.com>
2022-12-06 21:34:58 -0300

    last update for implement api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   openWeather: 'a5cf07b31ef30d7f5a16d1df55135522',
-  musicAPI: 'e2762bf16amsh93c9f365d08ea17p10b0d0jsn210bd939dde6'
+  musicAPI: 'e2762bf16amsh93c9f365d08ea17p10b0d0jsn210bd939dde6',
+  newsAPI: '67cf13a832ba3a73fce52db137b964ff'
 };
